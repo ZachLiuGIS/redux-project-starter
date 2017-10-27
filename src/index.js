60c74eec480273941cd72c92bff0c3a31c4062ca
ZachLiuGIS <zachliugis@gmail.com>
2017-10-26 22:45:53 -0400

    added redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import {createStore, applyMiddleware} from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/app";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <App/>
     </Provider>
     , document.getElementById("root"));
